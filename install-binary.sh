b145266b3d1f26e887bcc8941c59fe7ef782da79
Aleksandar Stojanov <me@fnd.works>
2023-10-25 00:59:20 +0200

    fix version format
    Signed-off-by: Aleksandar Stojanov <me@fnd.works>

--- install-binary.sh
+++ install-binary.sh
@@ -75,7 +75,7 @@ getDownloadURL() {
   fi
 
   # Setup Download Url
-  DOWNLOAD_URL="https://github.com/${PROJECT_GH}/releases/download/${version}/${PROJECT_NAME}-${OS}_${ARCH}-${version}.tgz"
+  DOWNLOAD_URL="https://github.com/${PROJECT_GH}/releases/download/${version}/${PROJECT_NAME}_${version}_${OS}_${ARCH}.tgz"
 }
 
 # downloadFile downloads the latest binary package and also the checksum
