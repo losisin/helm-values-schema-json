311748a97d4e6439d4c3c87269d7d9b795ada5f2
Aleksandar Stojanov <me@fnd.works>
2023-10-30 15:32:35 +0100

    move flags out of main function

--- schema_test.go
+++ schema_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"os"
 	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/losisin/go-jsonschema-generator"
@@ -202,6 +204,81 @@ func TestPrintMap(t *testing.T) {
 				t.Fatalf("Expected an error, but printMap succeeded")
 			case err != nil && !c.expectError:
 				t.Fatalf("Unexpected error: %v", err)
+			}
+		})
+	}
+}
+
+func TestParseFlagsCorrect(t *testing.T) {
+	var tests = []struct {
+		args []string
+		conf Config
+	}{
+		{[]string{"-input", "testdata/values.yaml"},
+			Config{input: multiStringFlag{"testdata/values.yaml"}, outputPath: "values.schema.json", draft: 2020, args: []string{}}},
+
+		{[]string{"-input", "values1.yaml testdata/values.yaml"},
+			Config{input: multiStringFlag{"values1.yaml testdata/values.yaml"}, outputPath: "values.schema.json", draft: 2020, args: []string{}}},
+
+		{[]string{"-input", "testdata/values.yaml", "-output", "my.schema.json", "-draft", "2019"},
+			Config{input: multiStringFlag{"testdata/values.yaml"}, outputPath: "my.schema.json", draft: 2019, args: []string{}}},
+	}
+
+	for _, tt := range tests {
+		t.Run(strings.Join(tt.args, " "), func(t *testing.T) {
+			conf, output, err := parseFlags("prog", tt.args)
+			if err != nil {
+				t.Errorf("err got %v, want nil", err)
+			}
+			if output != "" {
+				t.Errorf("output got %q, want empty", output)
+			}
+			if !reflect.DeepEqual(*conf, tt.conf) {
+				t.Errorf("conf got %+v, want %+v", *conf, tt.conf)
+			}
+		})
+	}
+}
+
+func TestParseFlagsUsage(t *testing.T) {
+	var usageArgs = []string{"-help", "-h", "--help"}
+
+	for _, arg := range usageArgs {
+		t.Run(arg, func(t *testing.T) {
+			conf, output, err := parseFlags("prog", []string{arg})
+			if err != flag.ErrHelp {
+				t.Errorf("err got %v, want ErrHelp", err)
+			}
+			if conf != nil {
+				t.Errorf("conf got %v, want nil", conf)
+			}
+			if !strings.Contains(output, "Usage of") {
+				t.Errorf("output can't find \"Usage of\": %q", output)
+			}
+		})
+	}
+}
+
+func TestParseFlagsError(t *testing.T) {
+	var tests = []struct {
+		args   []string
+		errstr string
+	}{
+		{[]string{"-input"}, "flag needs an argument"},
+		{[]string{"-draft", "foo"}, "invalid value"},
+	}
+
+	for _, tt := range tests {
+		t.Run(strings.Join(tt.args, " "), func(t *testing.T) {
+			conf, output, err := parseFlags("prog", tt.args)
+			if conf != nil {
+				t.Errorf("conf got %v, want nil", conf)
+			}
+			if !strings.Contains(err.Error(), tt.errstr) {
+				t.Errorf("err got %q, want to find %q", err.Error(), tt.errstr)
+			}
+			if !strings.Contains(output, "Usage of") {
+				t.Errorf("output got %q", output)
 			}
 		})
 	}
