391191c5125b3a6d8e912107aefd8897b79274ec
Aleksandar Stojanov <me@fnd.works>
2023-11-10 11:35:24 +0100

    fix url to plugin install

--- scripts/git-hook.sh
+++ scripts/git-hook.sh
@@ -3,7 +3,7 @@
 set -e
 
 if ! 'helm plugin list | grep -q "schema"' > /dev/null 2>&1; then
-    echo "Please install helm-values-schema-json plugin! https://github.com/losisin/helm-values-schema-json-action#github-actions"
+    echo "Please install helm-values-schema-json plugin! https://github.com/losisin/helm-values-schema-json#install"
 fi
 
 helm schema "${@}"
