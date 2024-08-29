bdeb471c9c8903c471dfe4dbdf0558b17b6aaf5e
Aleksandar Stojanov <me@fnd.works>
2024-08-29 17:36:16 +0200

    fix tests
    Signed-off-by: Aleksandar Stojanov <me@fnd.works>

--- pkg/generator_test.go
+++ pkg/generator_test.go
@@ -20,10 +20,11 @@ func TestGenerateJsonSchema(t *testing.T) {
 		Draft:      2020,
 		Indent:     4,
 		SchemaRoot: SchemaRoot{
-			ID:          "",
-			Ref:         "",
-			Title:       "",
-			Description: "",
+			ID:                   "https://example.com/schema",
+			Ref:                  "schema/product.json",
+			Title:                "Helm Values Schema",
+			Description:          "Schema for Helm values",
+			AdditionalProperties: BoolFlag{set: true, value: true},
 		},
 	}
 
