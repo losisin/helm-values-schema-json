5f786b0dccc4a891ef67bd646d97da05e1d0e5b7
Kalle Fagerberg <kalle.f8+github@proton.me>
2025-06-09 01:54:38 +0200

    Sorted commands in doc.go

--- testdata/doc.go
+++ testdata/doc.go
@@ -17,16 +17,16 @@ package testdata
 
 //go:generate go run .. --use-helm-docs --values helm-docs/values.yaml --output helm-docs/values.schema.json
 
-//go:generate go run .. --bundle=true --values bundle/fragment.yaml --output bundle/fragment.schema.json
-//go:generate go run .. --bundle=true --values bundle/fragment.yaml --output bundle/fragment-without-id.schema.json --bundle-without-id=true
-//go:generate go run .. --bundle=true --values bundle/namecollision.yaml --output bundle/namecollision.schema.json
-//go:generate go run .. --bundle=true --values bundle/nested.yaml --output bundle/nested.schema.json
-//go:generate go run .. --bundle=true --values bundle/nested.yaml --output bundle/nested-without-id.schema.json --bundle-without-id=true
-//go:generate go run .. --bundle=true --values bundle/simple.yaml --output bundle/simple.schema.json
 //go:generate go run .. --bundle=false --values bundle/simple.yaml --output bundle/simple-disabled.schema.json
-//go:generate go run .. --bundle=true --values bundle/simple.yaml --output bundle/simple-without-id.schema.json --bundle-without-id=true
-//go:generate go run .. --bundle=true --values bundle/simple.yaml --output bundle/simple-absolute-root.schema.json --bundle-root=/
-//go:generate go run .. --bundle=true --values bundle/yaml.yaml --output bundle/yaml.schema.json
-//go:generate go run .. --bundle=true --values bundle/simple.yaml --output bundle/simple-root-ref.schema.json --schema-root.ref ./bundle/simple-subschema.schema.json
-//go:generate go run .. --bundle=true --values bundle/multiple-values-1.yaml,bundle/multiple-values-2.yaml --schema-root.ref bundle/simple-subschema.schema.json --output bundle/multiple-values.schema.json
+//go:generate go run .. --bundle=true --values bundle/fragment.yaml --output bundle/fragment-without-id.schema.json --bundle-without-id=true
+//go:generate go run .. --bundle=true --values bundle/fragment.yaml --output bundle/fragment.schema.json
 //go:generate go run .. --bundle=true --values bundle/multiple-values-1.yaml,bundle/multiple-values-2.yaml --schema-root.ref bundle/simple-subschema.schema.json --output bundle/multiple-values-without-id.schema.json --bundle-without-id=true
+//go:generate go run .. --bundle=true --values bundle/multiple-values-1.yaml,bundle/multiple-values-2.yaml --schema-root.ref bundle/simple-subschema.schema.json --output bundle/multiple-values.schema.json
+//go:generate go run .. --bundle=true --values bundle/namecollision.yaml --output bundle/namecollision.schema.json
+//go:generate go run .. --bundle=true --values bundle/nested.yaml --output bundle/nested-without-id.schema.json --bundle-without-id=true
+//go:generate go run .. --bundle=true --values bundle/nested.yaml --output bundle/nested.schema.json
+//go:generate go run .. --bundle=true --values bundle/simple.yaml --output bundle/simple-absolute-root.schema.json --bundle-root=/
+//go:generate go run .. --bundle=true --values bundle/simple.yaml --output bundle/simple-root-ref.schema.json --schema-root.ref ./bundle/simple-subschema.schema.json
+//go:generate go run .. --bundle=true --values bundle/simple.yaml --output bundle/simple-without-id.schema.json --bundle-without-id=true
+//go:generate go run .. --bundle=true --values bundle/simple.yaml --output bundle/simple.schema.json
+//go:generate go run .. --bundle=true --values bundle/yaml.yaml --output bundle/yaml.schema.json
