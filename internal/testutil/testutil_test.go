2e31249b56cf1180a9be1b6dfeaa421037a67204
Aleksandar Stojanov <me@fnd.works>
2025-06-10 13:58:59 +0200

    bring back
    Signed-off-by: Aleksandar Stojanov <me@fnd.works>

--- internal/testutil/testutil_test.go
+++ internal/testutil/testutil_test.go
@@ -51,3 +51,24 @@ func TestResetFile(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, "much shorter content", string(content))
 }
+
+func TestCreateTempDir(t *testing.T) {
+	var dir string
+	t.Run("sub-test", func(t *testing.T) {
+		dir = CreateTempDir(t, "test-*.txt")
+		require.NotNil(t, dir)
+		require.DirExists(t, dir)
+	})
+	require.NoDirExists(t, dir)
+}
+
+func TestResetEnvAfterTest(t *testing.T) {
+	t.Setenv("foo", "bar")
+	t.Run("sub-test", func(t *testing.T) {
+		require.Equal(t, "bar", os.Getenv("foo"))
+		ResetEnvAfterTest(t)
+		require.NoError(t, os.Setenv("foo", "inner"))
+		require.Equal(t, "inner", os.Getenv("foo"))
+	})
+	require.Equal(t, "bar", os.Getenv("foo"))
+}
